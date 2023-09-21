c33bd88952a16b405c4a635a5c298e2c7f0a80ba
tiagolimar <tiagolima.r2@gmail.com>
2023-09-21 18:47:09 -0300

    config de rotas

--- desafio_.js
+++
@@ -1,8 +0,0 @@
-const fs = require("fs");
-const { setDefaultHighWaterMark } = require("stream");
-asdf
-asdfasdf
-a
-setDefaultHighWaterMark
-asdfasdf
-
--- server.js
+++ server.js
@@ -1,11 +1,21 @@
-const http = require('http')
+import http from 'http'
 
 const PORT = 3333
 const HOST = 'localhost'
 
 const server = http.createServer((req,res)=>{
-    res.end("foi?asdfasd")
+    console.log(req.method)
+
     
+    if (req.method == 'GET'){
+        if(req.url == '/usuarios'){
+            console.log('Entrou no /usuarios')
+            res.writeHead(200)
+            res.end("Entrou")
+        }else{
+            res.end("Entrous")
+        }
+    }
 })
 
 server.listen(PORT,HOST,()=>{
